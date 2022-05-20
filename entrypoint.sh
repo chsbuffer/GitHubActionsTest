39ce0e188926020e7365edd158a5c35e244c5e83
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2022-05-20 14:39:09 +0800

    abs mesa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,19 @@
 #! /bin/bash
 set -x
-
 pwd
+id
 
-pacman -Syu --noconfirm sudo git
+pacman --sync --refresh --sysupgrade --noconfirm sudo git
 
-useradd -m build -G wheel
-chown -R build /github/workspace /github/home
-echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd --create-home build --groups wheel
+chown --recursive build /github/workspace /github/home
+# echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 su --login build --command="\
- id &&\
+ id ;\
  export MAKEFLAGS=-j$(nproc) &&\
- cd $PWD &&\
- git clone https://aur.archlinux.org/directx-headers.git --depth 1 &&\
- cd directx-headers &&\
- makepkg -i --syncdeps --noconfirm &&\
- cd .. &&\
- git clone https://aur.archlinux.org/mesa-d3d12.git --depth 1 &&\
- cd mesa-d3d12 &&\
- makepkg --syncdeps --noconfirm --skippgpcheck\
+ git clone --branch packages/mesa --depth 1 https://github.com/archlinux/svntogit-packages.git mesa &&\
+ cd mesa/trunk &&\
+ makepkg --syncdeps --noconfirm --skipinteg &&\
+ cd .. ;\
 "
